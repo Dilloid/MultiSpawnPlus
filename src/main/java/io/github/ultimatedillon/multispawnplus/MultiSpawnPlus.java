0787ce68b6e5511b753525bcc68819c0b2ee2cf0
UltimateDillon <freetheenslaved@gmail.com>
2014-06-27 22:18:01 +0100

    Update Main Class

--- src/main/java/io/github/ultimatedillon/multispawnplus/MultiSpawnPlus.java
+++ src/main/java/io/github/ultimatedillon/multispawnplus/MultiSpawnPlus.java
@@ -1,10 +1,32 @@
 package io.github.ultimatedillon.multispawnplus;
 
+import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MultiSpawnPlus extends JavaPlugin {
 	@Override
 	public void onEnable() {
         new PlayerJoinListener(this);
+        
+        LoadConfig();
     }
+	
+	public void LoadConfig() {
+		World defaultWorld = Bukkit.getServer().getWorlds().get(0);
+		
+		int[] coords = {
+			defaultWorld.getSpawnLocation().getBlockX(),
+			defaultWorld.getSpawnLocation().getBlockY(),
+			defaultWorld.getSpawnLocation().getBlockZ()
+		};
+		
+		getConfig().addDefault("spawns.default.world", defaultWorld.getName());
+		getConfig().addDefault("spawns.default.X", coords[0]);
+		getConfig().addDefault("spawns.default.Y", coords[1]);
+		getConfig().addDefault("spawns.default.Z", coords[2]);
+		
+		getConfig().options().copyDefaults(true);
+        saveConfig();
+	}
 }
