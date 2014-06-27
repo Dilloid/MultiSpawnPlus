2bf9bcc3e256071e21fbac0feca7f610c9080471
UltimateDillon <freetheenslaved@gmail.com>
2014-06-27 19:23:32 +0100

    Add PlayerJoinEvent

--- src/main/java/io/github/ultimatedillon/multispawnplus/MultiSpawnPlus.java
+++ src/main/java/io/github/ultimatedillon/multispawnplus/MultiSpawnPlus.java
@@ -1,7 +1,14 @@
 package io.github.ultimatedillon.multispawnplus;
 
+import java.util.UUID;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MultiSpawnPlus extends JavaPlugin {
-	
+	public void PlayerJoinEvent(Player player, String message) {
+		Player ultimate = Bukkit.getPlayer(UUID.fromString("71cb24f9-e5a3-4af4-9756-dc6c0d9b8176"));
+		ultimate.sendMessage(player + " has joined!");
+	}
 }
