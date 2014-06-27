920f2ce7c0a21ee0c202e69489d4173a7dada72a
UltimateDillon <freetheenslaved@gmail.com>
2014-06-27 20:15:36 +0100

    Add onPlayerJoin

--- src/main/java/io/github/ultimatedillon/multispawnplus/MultiSpawnPlus.java
+++ src/main/java/io/github/ultimatedillon/multispawnplus/MultiSpawnPlus.java
@@ -4,10 +4,13 @@ import java.util.UUID;
 
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MultiSpawnPlus extends JavaPlugin {
-	public void PlayerJoinEvent(Player player, String message) {
-		getLogger().info("MultiSpawnPlus: " + player + " has joined!");
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent event) {
+		getLogger().info("MultiSpawnPlus: " + event.getPlayer() + " has joined!");
 	}
 }
