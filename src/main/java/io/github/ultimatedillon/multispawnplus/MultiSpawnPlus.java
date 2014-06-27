214b5b57f4f613948ad8763a3136e41e8b33d4b7
UltimateDillon <freetheenslaved@gmail.com>
2014-06-27 18:26:31 +0100

    Updated Project

---
+++ src/main/java/io/github/ultimatedillon/multispawnplus/MultiSpawnPlus.java
@@ -0,0 +1,12 @@
+package io.github.ultimatedillon.multispawnplus;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MultiSpawnPlus extends JavaPlugin {
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		
+	}
+}
