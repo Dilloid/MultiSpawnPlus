a9b7387280fd15b0e1e89d79be12ff631c59bda1
UltimateDillon <freetheenslaved@gmail.com>
2014-06-27 20:26:01 +0100

    Add PlayerJoinListener Class

--- src/main/java/io/github/ultimatedillon/multispawnplus/MultiSpawnPlus.java
+++ src/main/java/io/github/ultimatedillon/multispawnplus/MultiSpawnPlus.java
@@ -1,16 +1,10 @@
 package io.github.ultimatedillon.multispawnplus;
 
-import java.util.UUID;
-
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MultiSpawnPlus extends JavaPlugin {
-	@EventHandler
-	public void onPlayerJoin(PlayerJoinEvent event) {
-		getLogger().info("MultiSpawnPlus: " + event.getPlayer() + " has joined!");
-	}
+	@Override
+	public void onEnable() {
+        new PlayerJoinListener(this);
+    }
 }
---
+++ src/main/java/io/github/ultimatedillon/multispawnplus/PlayerJoinListener.java
@@ -0,0 +1,18 @@
+package io.github.ultimatedillon.multispawnplus;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public final class PlayerJoinListener implements Listener {
+	public PlayerJoinListener(MultiSpawnPlus plugin) {
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
+    }
+	
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent event) {
+		Bukkit.getServer().getLogger().info("MultiSpawnPlus: " + event.getPlayer().getName() + " has joined!");
+		event.getPlayer().sendMessage("You joined the server.");
+	}
+}
