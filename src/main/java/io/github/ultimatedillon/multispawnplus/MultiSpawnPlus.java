6aeb8fa47b0dc4cdaff24f6a800652997e459f7b
UltimateDillon <freetheenslaved@gmail.com>
2014-06-28 02:58:10 +0100

    Add Random Spawn list

--- src/main/java/io/github/ultimatedillon/multispawnplus/MultiSpawnPlus.java
+++ src/main/java/io/github/ultimatedillon/multispawnplus/MultiSpawnPlus.java
@@ -1,6 +1,9 @@
 package io.github.ultimatedillon.multispawnplus;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Set;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -30,13 +33,32 @@ public final class MultiSpawnPlus extends JavaPlugin {
 		
 		if (!new File(getDataFolder(), "config.yml").exists()) {
 			getConfig().addDefault("spawns.default.world", defaultWorld.getName());
+			getConfig().addDefault("spawns.default.allow-random-spawn", true);
 			getConfig().addDefault("spawns.default.X", coords[0]);
 			getConfig().addDefault("spawns.default.Y", coords[1]);
 			getConfig().addDefault("spawns.default.Z", coords[2]);
 			
 			getConfig().options().copyDefaults(true);
-	        saveConfig();
+			saveConfig();
 		}
+		
+		getConfig().set("random-spawns", null);
+		Set<String> spawnList = getConfig().getConfigurationSection("spawns").getKeys(false);
+		String[] spawns = spawnList.toArray(new String[spawnList.size()]);
+		ArrayList<String> allowedList = new ArrayList<String>();
+		
+		for (int i = 0; i < spawns.length; i++) {
+			if (getConfig().getBoolean("spawns." + spawns[i] + ".allow-random-spawn") == true) {
+				if (spawns[i] != null && spawns[i] != "null") {
+					allowedList.add(spawns[i]);
+					getLogger().info("MultiSpawnPlus - Item Added: " + spawns[i]);
+				}
+			}
+		}
+		
+		String[] allowed = allowedList.toArray(new String[allowedList.size()]);
+		getConfig().set("random-spawns.allowed", Arrays.asList(allowed));
+		saveConfig();
 	}
 	
 	@Override
@@ -50,18 +72,25 @@ public final class MultiSpawnPlus extends JavaPlugin {
 				
 				if (args[0].equalsIgnoreCase("add")) {
 					if (player.hasPermission("multispawnplus.delete")) {
-						if (args.length < 2) {
-							player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cPlease enter a name for this spawnpoint."));
-						} else if (args.length > 2) {
+						if (args.length < 3) {
+							player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cPlease enter a name for this spawnpoint, and whether this new players can spawn here."));
+							player.sendMessage("Usage: /multispawnplus add <name> <[true|false]>");
+						} else if (args.length > 3) {
 							player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cToo many arguments!"));
-							return false;
+							player.sendMessage("Usage: /multispawnplus add <name> <[true|false]>");
 						} else if (!getConfig().contains("spawns." + args[1])) {
-							getConfig().set("spawns." + args[1] + ".world", player.getWorld().getName());
-							getConfig().set("spawns." + args[1] + ".X", player.getLocation().getBlockX());
-							getConfig().set("spawns." + args[1] + ".Y", player.getLocation().getBlockY());
-							getConfig().set("spawns." + args[1] + ".Z", player.getLocation().getBlockZ());
-							saveConfig();
-							player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&bSpawnpoint &f" + args[1] + " &bhas been created!"));
+							if (args[2] != "true" || args[2] != "false") {
+								getConfig().set("spawns." + args[1] + ".world", player.getWorld().getName());
+								getConfig().set("spawns." + args[1] + ".allow-random-spawn", new Boolean(args[2]));
+								getConfig().set("spawns." + args[1] + ".X", player.getLocation().getBlockX());
+								getConfig().set("spawns." + args[1] + ".Y", player.getLocation().getBlockY());
+								getConfig().set("spawns." + args[1] + ".Z", player.getLocation().getBlockZ());
+								saveConfig();
+								player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&bSpawnpoint &f" + args[1] + " &bhas been created!"));
+							} else {
+								player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cSyntax Error!"));
+								player.sendMessage("Usage: /multispawnplus add <name> <[true|false]>");
+							}
 						} else {
 							player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cA spawnpoint with that name already exists!"));
 							player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cUse &f/multispawnplus delete <name> &bto delete existing spawnpoints."));
@@ -76,7 +105,7 @@ public final class MultiSpawnPlus extends JavaPlugin {
 							player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cPlease enter a spawnpoint to delete."));
 						} else if (args.length > 2) {
 							player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cToo many arguments!"));
-							return false;
+							player.sendMessage("Usage: /multispawnplus delete <name>");
 						} else {
 							if (!getConfig().contains(args[1])) {
 								getConfig().set("spawns." + args[1], null);
@@ -96,7 +125,7 @@ public final class MultiSpawnPlus extends JavaPlugin {
 							player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cPlease enter a spawnpoint."));
 						} else if (args.length > 2) {
 							player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cToo many arguments!"));
-							return false;
+							player.sendMessage("Usage: /multispawnplus spawn <name>");
 						} else {
 							if (!getConfig().contains(args[1])) {
 								World world = Bukkit.getWorld(getConfig().get("spawns." + args[1] + ".world").toString());
